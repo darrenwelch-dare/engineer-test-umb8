4288f786a83d06b98f4bf29228101726556308b0
Darren Welch <darren.welch@thisisdare.com>
2021-12-16 17:57:30 +0000

    files checked in

---
+++ Dare-test-umb8/Controllers/MemberController.cs
@@ -0,0 +1,52 @@
+﻿using Dare_test_umb8.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using Umbraco.Web.Mvc;
+
+namespace Dare_test_umb8.Controllers
+{
+    public class MemberController : SurfaceController
+    {
+        public ActionResult RenderLogin()
+        {
+            return PartialView("_Login", new LoginModel());
+        }
+
+        [System.Web.Http.HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitLogin(LoginModel model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.Username, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username, false);
+                    return Redirect("/to-do/");
+                 
+                }
+                else
+                {
+                    ModelState.AddModelError("", "The username or password provided is incorrect.");
+                }
+            }
+            return CurrentUmbracoPage();
+        }
+
+        public ActionResult RenderLogout()
+        {
+            return PartialView("_Logout", null);
+        }
+
+        public ActionResult SubmitLogout()
+        {
+            TempData.Clear();
+            Session.Clear();
+            FormsAuthentication.SignOut();
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ Dare-test-umb8/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Dare_test_umb8")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Dare_test_umb8")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18a9923f-9ac2-438b-97ac-0d5e27686f8e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
